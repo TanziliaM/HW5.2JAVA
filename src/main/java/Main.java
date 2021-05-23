f030eb1775b289bce6aa64d6914fca07ec1c2f5d
Tanzilia Musatova <musatova.tanzilia@gmail.com>
2021-05-23 23:01:48 +0300

    Error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,7 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    long actual = service.calculate(amount, registered);
-    long passed = actual;
-
-    System.out.println(passed);
+  
 
   }
 }
