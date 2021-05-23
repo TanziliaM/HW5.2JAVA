428b30c357b06dc27225321f6f5d4ef1698d076b
Tanzilia Musatova <musatova.tanzilia@gmail.com>
2021-05-23 23:04:21 +0300

    bag fix. close issue #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,10 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-  
+    long actual = service.calculate(amount, registered);
+    long passed = actual;
+
+    System.out.println(passed);
 
   }
 }
